2f12d678c81edf5353e1d373f80417d833cd9037
RohitoOo <bhambhani.ba@gmail.com>
2018-04-21 00:27:56 +0530

    Second Commit

--- app.js
+++ app.js
@@ -1,46 +1,89 @@
-const express = require('express')
-const path = require('path')
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser')
+mongoose.connect('mongodb://localhost/nodekb');
+let db = mongoose.connection;
+
+// Check connection
+
+db.once('open' , function (){
+console.log("Connected to MongoDB")
+});
+
+// Check for DB errors
+
+db.on('error' , function (err){
+  console.log("err")
+});
+
+
 // Init app
+
 const app = express();
+
+
+// Bring In Models
+let Article = require('./models/article') ;
+
 
 // Load View Engine
 
 app.set('views' , path.join(__dirname, 'views'));
 app.set('view engine' , 'pug');
 
+// BODY Parse required Middle ware (github)
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+
+
+// Set Public Folder
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// This data will be fetched from the MongoDB
+// let articles = [
+//   {
+//     id:1,
+//     title:"Article 1",
+//     author:'Rohito The Author',
+//     body:"This is the first article"
+//   },
+//   {
+//     id:2,
+//     title:"Article 2",
+//     author:'Sanju The Author',
+//     body:"This is the second article"
+//   },
+//   {
+//     id:3,
+//     title:"Article 3",
+//     author:'Lavish The Author',
+//     body:"This is the third article"
+//   }
+// ];
 
 
 // Home Route
 
 
-// This data will be fetched from the MongoDB
-let articles = [
-  {
-    id:1,
-    title:"Article 1",
-    author:'Rohito The Author',
-    body:"This is the first article"
-  },
-  {
-    id:2,
-    title:"Article 2",
-    author:'Sanju The Author',
-    body:"This is the second article"
-  },
-  {
-    id:3,
-    title:"Article 3",
-    author:'Lavish The Author',
-    body:"This is the third article"
-  }
-];
 
 app.get('/' , function (req,res){
 
+  Article.find({} , function (err , articles){
+if(err){
+
+  console.log(err)
+} else{
   res.render("index" , {
     titles:'Helloooo!',
-
     articles: articles
+});
+}
+
 });
 });
 
@@ -56,7 +99,28 @@ app.get('/articles/add' , function(req,res){
 })
 
 
-app.listen(3000 , function(){
 
+// Create Submit post route
+
+
+app.post('/articles/add' , function (req, res) {
+
+let article = new Article();
+
+article.title = req.body.title;
+article.body = req.body.body;
+article.author = req.body.author;
+
+article.save(function(err){
+  if(err){
+    console.log(err);
+    return;
+  }else{
+    res.redirect('/')
+  }
+})
+});
+
+app.listen(3000 , function(){
   console.log("Server started on Port 3000...")
 })
